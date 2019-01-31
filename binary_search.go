e1d6644f7224f5c8f6473ca58991db0df0e6b767
1 <1>
2019-01-31 10:30:46 +0800

    update binary search

--- binary_search.go
+++ binary_search.go
@@ -20,7 +20,7 @@ func IndexOf (array []int, key int) (int) {
 
 func main () {
 	var array = []int{1,2,3,4,5,7,8,9,11,13}
-	var index = IndexOf(array, 7)
+	var index = IndexOf(array, 71)
 	if index >= 0 {
 		fmt.Println(fmt.Sprintf("the key at index %d of array", index))
 	} else {
