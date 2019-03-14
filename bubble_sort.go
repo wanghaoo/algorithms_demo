71614db0252c406df5df4ff0ed7a53eaafb2129d
1 <1>
2019-03-14 11:12:16 +0800

    add

--- bubble_sort.go
+++ bubble_sort.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 
 func bubble_sort(array []int)([]int) {
-  for i := 0; i < len(array); i++ {
-  	for j := i + 1; j < len(array); j++ {
-  		if array[i] < array[j] {
-			array[j], array[i] = array[i], array[j]
+	for i := 0; i < len(array); i++ {
+		for j := i + 1; j < len(array); j++ {
+			if array[i] < array[j] {
+				array[j], array[i] = array[i], array[j]
+			}
 		}
 	}
-  }
-  return array
+	return array
 }
 
 func main() {
--- stack.go
+++ stack.go
@@ -1,1 +1,62 @@
 package main
+
+import (
+  "fmt"
+)
+
+type Stack struct {
+  First *StackNode
+  Count int
+  Size int
+}
+
+type StackNode struct {
+  Value int
+  Next *StackNode
+}
+
+func NewStack(size int) (*Stack) {
+  var stack = new(Stack)
+  stack.Size = size
+  return stack
+}
+
+func (s *Stack) len()(int) {
+  return s.Count
+}
+
+func (s *Stack)isFull()(bool) {
+  return s.Size == s.Count
+}
+
+func (s *Stack) pull(value int) {
+  if s.isFull() {
+    return
+  }
+  var oldFirst = s.First
+  var node = new(StackNode)
+  node.Value = value
+  node.Next = oldFirst
+  s.Count += 1
+  s.First = node
+}
+
+func (s *Stack) pop() (int) {
+  var oldFirst = s.First
+  var newFirst = s.First.Next
+  s.First = newFirst
+  s.Count -= 1
+  return oldFirst.Value
+}
+
+func main() {
+ var iArray = []int{1,2, 3,4,5,6,7}
+ var stack = NewStack(5)
+  for _, i := range iArray {
+    stack.pull(i)
+  }
+  fmt.Println("stack count :", stack.Count)
+  for stack.Count > 0{
+    fmt.Println(stack.pop())
+  }
+}
