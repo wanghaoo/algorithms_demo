dcc6b01cffbde607f4ebff128fc7c71892938165
wang.yu.lion@gmail.com <wang.yu.lion@gmail.com>
2019-04-15 00:34:55 +0800

    add

--- choise_sort.go
+++ choise_sort.go
@@ -4,16 +4,18 @@ import "fmt"
 
 func main () {
   var arr = []int{4,3,2,6,8,1,11,56,3,45,7,2,5,7,9,3}
-  for i, _ := range arr {
-    var min = i
-    for _, j := range arr {
-      if min > j {
-        min = j
+  for i, a := range arr {
+    var min = a
+    var innerIndex = i
+    for n := i ; n < len(arr); n++ {
+      if min > arr[n] {
+        min = arr[n]
+        innerIndex = n
       }
     }
-    arr[i] = min
+    arr[i],arr[innerIndex] = arr[innerIndex],arr[i]
   }
   for _, a := range arr {
-    fmt.Print(a)
+    fmt.Print(a, ",")
   }
 }
