0e36d0ce6e399eb89bbb2660e986ddafdfb7cfdf
1 <1>
2019-04-18 11:48:59 +0800

    add

--- merge_sort.go
+++ merge_sort.go
@@ -3,10 +3,9 @@ package main
 import "fmt"
 
 func main() {
-  //var array = []int{1, 54, 4, 67, 8, 4577, 34, 77, 35, 889, 567, 33, 5778, 799, 44, 56, 78, 443, 567, 4456}
-  var array = []int{1, 54, 4, 67}
+  var array = []int{1, 54, 4, 67, 8, 4577, 34, 77, 35, 889, 567, 33, 5778, 799, 44, 56, 78, 443, 567, 4456}
   sort(array)
-  //fmt.Println(array)
+  fmt.Println(array)
 }
 
 func sort(array []int) {
@@ -20,15 +19,11 @@ func splitArraySort(a []int, aux []int, start, end int) {
   }
   mid := start + (end - start) / 2
   splitArraySort(a, aux, start, mid)
-  fmt.Println("f mid: ", mid)
   splitArraySort(a, aux, mid + 1, end)
-  fmt.Println("sec mid: ", mid + 1)
-  //merge(a, aux, start, mid, end)
+  merge(a, aux, start, mid, end)
 }
 
-//var array = []int{1, 54, 4, 67}
 func merge(a []int, aux []int, s, m, e int) ([]int) {
-  //fmt.Println("s: ", s,"m: ",  m,"e: ", e)
   for k := s; k <= e; k++ {
     aux[k] = a[k]
   }
@@ -39,16 +34,15 @@ func merge(a []int, aux []int, s, m, e int) ([]int) {
       a[k] = aux[j]
       j++
     } else if j > e {
-      i++
       a[k] = aux[i]
+      i++
     } else if aux[j] < aux[i] {
       a[k] = aux[j]
       j++
     } else {
-      i++
       a[k] = aux[i]
+      i++
     }
   }
-  fmt.Println(a)
   return a
 }
